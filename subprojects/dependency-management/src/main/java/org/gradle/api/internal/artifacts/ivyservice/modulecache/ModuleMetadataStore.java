99d0308d62f8b6fd42cd7e1ec08a79d4ec5d68ea
Cedric Champeau <cedric@gradle.com>
2018-04-26 14:41:25 +0200

    Experiment with interning strings in module metadata

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/modulecache/ModuleMetadataStore.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/modulecache/ModuleMetadataStore.java
@@ -15,6 +15,7 @@
  */
 package org.gradle.api.internal.artifacts.ivyservice.modulecache;
 
+import com.google.common.collect.Maps;
 import org.gradle.api.Action;
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier;
 import org.gradle.api.internal.artifacts.ImmutableModuleIdentifierFactory;
@@ -23,18 +24,26 @@ import org.gradle.internal.component.external.model.ModuleComponentResolveMetada
 import org.gradle.internal.component.external.model.MutableModuleComponentResolveMetadata;
 import org.gradle.internal.resource.local.LocallyAvailableResource;
 import org.gradle.internal.resource.local.PathKeyFileStore;
+import org.gradle.internal.serialize.Decoder;
 import org.gradle.internal.serialize.kryo.KryoBackedDecoder;
 import org.gradle.internal.serialize.kryo.KryoBackedEncoder;
 
+import javax.annotation.Nullable;
+import java.io.Closeable;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 public class ModuleMetadataStore {
 
     private final PathKeyFileStore metaDataStore;
     private final ModuleMetadataSerializer moduleMetadataSerializer;
     private final ImmutableModuleIdentifierFactory moduleIdentifierFactory;
+    private final Map<String, String> internedStrings = Maps.newConcurrentMap();
 
     public ModuleMetadataStore(PathKeyFileStore metaDataStore, ModuleMetadataSerializer moduleMetadataSerializer, ImmutableModuleIdentifierFactory moduleIdentifierFactory) {
         this.metaDataStore = metaDataStore;
@@ -47,7 +56,7 @@ public class ModuleMetadataStore {
         final LocallyAvailableResource resource = metaDataStore.get(filePath);
         if (resource != null) {
             try {
-                KryoBackedDecoder decoder = new KryoBackedDecoder(new FileInputStream(resource.getFile()));
+                StringDeduplicatingDecoder decoder = new StringDeduplicatingDecoder(new KryoBackedDecoder(new FileInputStream(resource.getFile())));
                 try {
                     return moduleMetadataSerializer.read(decoder, moduleIdentifierFactory);
                 } finally {
@@ -83,4 +92,98 @@ public class ModuleMetadataStore {
         return moduleComponentIdentifier.getGroup() + "/" + moduleComponentIdentifier.getModule() + "/" + moduleComponentIdentifier.getVersion() + "/" + componentId.getRepositoryId() + "/descriptor.bin";
     }
 
+    private String intern(String str) {
+        if (str == null) {
+            return null;
+        }
+        String interned = internedStrings.get(str);
+        if (interned == null) {
+            internedStrings.put(str, str);
+            return str;
+        }
+        return interned;
+    }
+
+    private class StringDeduplicatingDecoder implements Decoder, Closeable {
+        private final Decoder delegate;
+
+        private StringDeduplicatingDecoder(Decoder delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public InputStream getInputStream() {
+            return delegate.getInputStream();
+        }
+
+        @Override
+        public long readLong() throws EOFException, IOException {
+            return delegate.readLong();
+        }
+
+        @Override
+        public long readSmallLong() throws EOFException, IOException {
+            return delegate.readSmallLong();
+        }
+
+        @Override
+        public int readInt() throws EOFException, IOException {
+            return delegate.readInt();
+        }
+
+        @Override
+        public int readSmallInt() throws EOFException, IOException {
+            return delegate.readSmallInt();
+        }
+
+        @Override
+        public boolean readBoolean() throws EOFException, IOException {
+            return delegate.readBoolean();
+        }
+
+        @Override
+        public String readString() throws EOFException, IOException {
+            return intern(delegate.readString());
+        }
+
+        @Override
+        @Nullable
+        public String readNullableString() throws EOFException, IOException {
+            String str = delegate.readNullableString();
+            if (str != null) {
+                str = intern(str);
+            }
+            return str;
+        }
+
+        @Override
+        public byte readByte() throws EOFException, IOException {
+            return delegate.readByte();
+        }
+
+        @Override
+        public void readBytes(byte[] buffer) throws EOFException, IOException {
+            delegate.readBytes(buffer);
+        }
+
+        @Override
+        public void readBytes(byte[] buffer, int offset, int count) throws EOFException, IOException {
+            delegate.readBytes(buffer, offset, count);
+        }
+
+        @Override
+        public byte[] readBinary() throws EOFException, IOException {
+            return delegate.readBinary();
+        }
+
+        @Override
+        public void skipBytes(long count) throws EOFException, IOException {
+            delegate.skipBytes(count);
+        }
+
+        @Override
+        public void close() throws IOException {
+            ((Closeable)delegate).close();
+        }
+    }
 }
